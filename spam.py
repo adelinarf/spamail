63df2f908be496db375790d99ed6e989c14c5620
Adelina Figueira <51302331+adelinarf@users.noreply.github.com>
2020-09-03 21:51:45 -0400

    Add files via upload

--- spam.py
+++ spam.py
@@ -7,66 +7,66 @@ from math import floor
 
 #### Classes:
 class testData:
-        """Record type that holds all the important data. """
-        def __init__(self):
-                """Note: mq means message quantity"""
-                self.test = parseFile()
-                self.mq = getSubLength(self.test)
-                self.train_data = list(map(lambda xs: getRandomSample(xs,0.8),self.test))
-                self.mq_train = getSubLength(self.train_data)
-                self.test_data = [list(set(self.test[0]) - set(self.train_data[0])),list(set(self.test[1]) - set(self.train_data[1]))]
-                self.mq_test = getSubLength(self.train_data)
+	"""Record type that holds all the important data. """
+	def __init__(self):
+		"""Note: mq means message quantity"""
+		self.test = parseFile()
+		self.mq = getSubLength(self.test)
+		self.train_data = list(map(lambda xs: getRandomSample(xs,0.8),self.test))
+		self.mq_train = getSubLength(self.train_data)
+		self.test_data = [list(set(self.test[0]) - set(self.train_data[0])),list(set(self.test[1]) - set(self.train_data[1]))]
+		self.mq_test = getSubLength(self.train_data)
 
 
 ### Aux Functions:
 #def concat(l: List[List[Any]]) -> List[Any]:
 def concat(l):
-        return [s for xs in l for s in xs]
+	return [s for xs in l for s in xs]
 
 def getSubLength(xs):
-        return reduce(lambda x,y:x+y, map(len,xs))
+	return reduce(lambda x,y:x+y, map(len,xs))
 
 def getRandomSample(xs, p):
-        return random.sample(xs,int(floor(len(xs)*p)))
+	return random.sample(xs,int(floor(len(xs)*p)))
 
 ### Helper Functions:
 
 #def cleanString(message: str) -> str:
 def cleanString(message):
-        """Returns a curated string without symbols, multiple spaces, leading/trealing 
-        spaces and lowercased."""
-        return message.replace('W+',' ').replace('\s+',' ').lower().strip()
+	"""Returns a curated string without symbols, multiple spaces, leading/trealing 
+	spaces and lowercased."""
+	return message.replace('W+',' ').replace('\s+',' ').lower().strip()
 
 #def parseFile() -> List[List[Any]]:
 def parseFile():
-        """" Reads the spam collection sample and 
-        Returns a list whose first element contains all the spam messages
-        and its second element contains all the non spam messages"""
-        f = open("SMSSpamCollection")
-        messages = [[],[]]
-        for line in f:
-                parse = line.split("\t")
-                if (parse[0] == "spam"):
-                        messages[0] += [parse[1]]
-                else:
-                        messages[1] += [parse[1]]
-        f.close()
-        return messages
+	"""" Reads the spam collection sample and 
+	Returns a list whose first element contains all the spam messages
+	and its second element contains all the non spam messages"""
+	f = open("SMSSpamCollection")
+	messages = [[],[]]
+	for line in f:
+			parse = line.split("\t")
+			if (parse[0] == "spam"):
+					messages[0] += [parse[1]]
+			else:
+					messages[1] += [parse[1]]
+	f.close()
+	return messages
 
 #def N_wi_spam(word: str,message_spam: List[str]) -> int:
 def N_wi_spam(word,message_spam):                                        
-        words = concat(map(lambda s: s.split(),message_spam))
-        f = lambda w: 1 if (w==word) else 0
-        return reduce(lambda x,y: x+y,map(f,words))
+	words = concat(map(lambda s: s.split(),message_spam))
+	f = lambda w: 1 if (w==word) else 0
+	return reduce(lambda x,y: x+y,map(f,words))
                                         
 ### Main Functions:
 
 def naive_bayes(message):
-        data = testData() # Test data.
-        words_not_spam = concat(map(lambda s: s.plit(),data.train_data[1]))
-        words_spam = concat(map(lambda s: s.plit(),data.train_data[0]))
+	data = testData() # Test data.
+	words_not_spam = concat(map(lambda s: s.split(),data.train_data[1]))
+	words_spam = concat(map(lambda s: s.split(),data.train_data[0]))
 	words =  words_not_spam + words_spam
-        vocabulary = list(set(words))
+	vocabulary = list(set(words))
 	N_vocabulary = len(vocabulary)  # Number of unique words in the dataset
 	N_not_spam=len(words_not_spam)	
 	N_spam=len(words_spam)  # Number of words in spam mails
@@ -77,12 +77,10 @@ def naive_bayes(message):
 	
 	message=data.train_data[1]
 	
-	
 	message_spam=data.train_data[0]
 	
 	
-	
-	q=classify(message,word,words_spam,words_not_spam,alpha,N_spam,N_vocabulary,P_spam,P_not_spam,N_not_spam,message_spam)
+	q=classify(message,words_spam,words_not_spam,alpha,N_spam,N_vocabulary,P_spam,P_not_spam,N_not_spam,message_spam)
 	print (q)
 	return (q)
 	
@@ -111,7 +109,7 @@ def not_spam_(word,words_not_spam,P_not_spam,message_spam,alpha,N_vocabulary,N_n
 	else:
 		return 1
 
-def classify(message,word,words_spam,words_not_spam,alpha,N_spam,N_vocabulary,P_spam,P_not_spam,N_not_spam,message_spam):
+def classify(message,words_spam,words_not_spam,alpha,N_spam,N_vocabulary,P_spam,P_not_spam,N_not_spam,message_spam):
 	p_spam_message=P_spam
 	p_not_spam_message=P_not_spam
 	for word in message:
@@ -125,7 +123,7 @@ def classify(message,word,words_spam,words_not_spam,alpha,N_spam,N_vocabulary,P_
 		"Can't be classified"
 	
 	
-def spam():
+def spam(datas):
 	data = testData()
 	datas=data.test
 	mensajes=[]
@@ -154,5 +152,4 @@ def spam():
 		if mensajes[1][i] in datas[1]:    
 			goodnot+=1
 	return (good+goodnot)
-
-#spam()
+			
