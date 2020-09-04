2dc0f05325a2172feecf26cd88b6124badba183d
Initial-Mockingbird <Daniel.Andres.Pinto@gmail.com>
2020-09-03 23:03:13 -0400

    fixed integer division errors

--- spam.py
+++ spam.py
@@ -12,7 +12,7 @@ class testData:
 		"""Note: mq means message quantity"""
 		self.test = parseFile()
 		self.mq = getSubLength(self.test)
-		self.train_data = list(map(lambda xs: getRandomSample(xs,0.8),self.test))
+		self.train_data = list(map(lambda xs: getRandomSample(xs,0.99),self.test))
 		self.mq_train = getSubLength(self.train_data)
 		self.test_data = [list(set(self.test[0]) - set(self.train_data[0])),list(set(self.test[1]) - set(self.train_data[1]))]
 		self.mq_test = getSubLength(self.train_data)
@@ -54,76 +54,62 @@ def parseFile():
 	return messages
 
 #def N_wi_spam(word: str,message_spam: List[str]) -> int:
-def N_wi_spam(word,message_spam):                                        
-	words = concat(map(lambda s: s.split(),message_spam))
+def N_wi_spam(word,message):                                        
+	words = concat(map(lambda s: s.split(),message))
 	f = lambda w: 1 if (w==word) else 0
 	return reduce(lambda x,y: x+y,map(f,words))
-                                        
+
+
+def p_wi_(word,words,message_,alpha,N_vocabulary,N_):
+        if word in words:
+                return (N_wi_spam(word,message_)+alpha)/float((N_+ alpha*N_vocabulary))
+        else:
+                return 1.0
 ### Main Functions:
 
 def naive_bayes(message):
 	data = testData() # Test data.
-	words_not_spam = concat(map(lambda s: s.split(),data.train_data[1]))
-	words_spam = concat(map(lambda s: s.split(),data.train_data[0]))
-	words =  words_not_spam + words_spam
-	vocabulary = list(set(words))
+	words_not_spam = concat(map(lambda s: s.split(),data.train_data[1])) # Words in all the not spam messages
+	words_spam = concat(map(lambda s: s.split(),data.train_data[0]))  # Words in all the spam messages
+	words =  words_not_spam + words_spam # total words
+	vocabulary = list(set(words)) # unique words
 	N_vocabulary = len(vocabulary)  # Number of unique words in the dataset
-	N_not_spam=len(words_not_spam)	
+	N_not_spam=len(words_not_spam)	# nomber of words in not spam mails
 	N_spam=len(words_spam)  # Number of words in spam mails
 
-	alpha=1
-	P_spam = len(data.train_data[0])/data.mq_train
-	P_not_spam = len(data.train_data[1])/data.mq_train
+	alpha=1.0
+	P_spam = len(data.train_data[0])/float(data.mq_train)
+	P_not_spam = len(data.train_data[1])/float(data.mq_train)
 	
-	message=data.train_data[1]
+	message_not_spam=data.train_data[1]
 	
 	message_spam=data.train_data[0]
 	
 	
-	q=classify(message,words_spam,words_not_spam,alpha,N_spam,N_vocabulary,P_spam,P_not_spam,N_not_spam,message_spam)
+	q=classify(message,words_spam,words_not_spam,alpha,N_spam,N_vocabulary,P_spam,P_not_spam,N_not_spam,message_spam,message_not_spam)
 	print (q)
 	return (q)
 	
-#def N_wi_spam(word,message_spam):
-#	contador=0
-#	for i in range(len(message_spam)):
-#		if word in message_spam[i]:
-#			w=message_spam[i].split()
-#			for i in range(len(w)):
-#				if word==w[i]:
-#					contador+=1
-#				else:
-#					i=i+1
-#	return contador	
-	
-def spam_(word,words_spam,alpha,N_spam,N_vocabulary,message_spam,P_spam):
-	if word in words_spam:
-		return (N_wi_spam(word,message_spam)+alpha)/(N_spam+(alpha*N_vocabulary))
-	else:
-		return 1
-	
-	
-def not_spam_(word,words_not_spam,P_not_spam,message_spam,alpha,N_vocabulary,N_not_spam):
-	if word in words_not_spam:
-		return (N_wi_spam(word,message_spam)+alpha)/(N_not_spam+(alpha*N_vocabulary))
-	else:
-		return 1
-
-def classify(message,words_spam,words_not_spam,alpha,N_spam,N_vocabulary,P_spam,P_not_spam,N_not_spam,message_spam):
+       
+def classify(message,words_spam,words_not_spam,alpha,N_spam,N_vocabulary,P_spam,P_not_spam,N_not_spam,message_spam,message_not_spam):
 	p_spam_message=P_spam
 	p_not_spam_message=P_not_spam
 	for word in message:
-		p_spam_message*=spam_(word,words_spam,alpha,N_spam,N_vocabulary,message_spam,P_spam)
-		p_not_spam_message*=not_spam_(word,words_not_spam,P_not_spam,message_spam,alpha,N_vocabulary,N_not_spam)
+                p_spam_message     *= p_wi_(word,words_spam,message_spam,alpha,N_vocabulary,N_spam)
+                p_not_spam_message *= p_wi_(word,words_not_spam,message_not_spam,alpha,N_vocabulary,N_not_spam)
+                #p_spam_message*=spam_(word,words_spam,alpha,N_spam,N_vocabulary,message_spam,P_spam)
+		#p_not_spam_message*=not_spam_(word,words_not_spam,P_not_spam,message_spam,alpha,N_vocabulary,N_not_spam)
+        print(p_spam_message)
+        print(p_not_spam_message)
 	if p_not_spam_message>p_spam_message:
 		return "Not spam"
 	elif p_not_spam_message<p_spam_message:
 		return "Spam"
 	else:
-		"Can't be classified"
+		return "Can't be classified"
 	
 	
-def spam(datas):
+def spam():
 	data = testData()
 	datas=data.test
 	mensajes=[]
